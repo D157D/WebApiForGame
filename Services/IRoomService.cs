ac62742484775b21ba0ed9c5c1c97f81c0772925
Khắc Duy <188033594+D157D@users.noreply.github.com>
2026-03-20 03:00:11 +0700

    Update IRoomService.cs

--- Services/IRoomService.cs
+++ Services/IRoomService.cs
@@ -1,8 +1,11 @@
+using System.Collections.Generic;
+
 namespace Crazy_Lobby.Services
 {
     public interface IRoomService
     {
         bool IsHost(string roomId, string playerId);
         void RemoveRoom(string roomId);
+        IEnumerable<string> GetPlayerIdsInRoom(string roomId);
     }
 }
